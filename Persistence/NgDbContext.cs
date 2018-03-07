577931fee4e7a20c0221309837fa95119c3c4be6
martin304 <ll-wang@hotmail.com>
2018-03-07 13:35:18 +0800

    E14 add key to VehicleFeature

--- Persistence/NgDbContext.cs
+++ Persistence/NgDbContext.cs
@@ -6,12 +6,16 @@ namespace angular2.Persistence
 {
     public class NgDbContext:DbContext
     {
+        public DbSet<Make> Makes{get;set;}
+        public DbSet<Feature> Features{get;set;}
         public NgDbContext(DbContextOptions<NgDbContext> options)
         :base(options)
         {
             
         }
-        public DbSet<Make> Makes{get;set;}
-        public DbSet<Feature> Features{get;set;}
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+          modelBuilder.Entity<VehicleFeature>().HasKey(vf=> 
+             new {vf.VehicleId,vf.FeatureId});
+        }
     }
 }
