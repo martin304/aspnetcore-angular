0d62cc80c363d10d1bb1f9ef67fda5122652586e
martin304 <ll-wang@hotmail.com>
2018-03-07 13:07:17 +0800

    E12 modify Vehicle model

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,10 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace angular2.Models
 {
     public class Vehicle
     {
         public int Id { get; set; }
-        public string Name{get;set;}
         public int ModelId{get;set;}
         public Model Model{get;set;}
+        public bool IsRegistered{get;set;}
+       
+        [StringLength(255)]
+        public string contactEmail { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string contactName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string contactPhone { get; set; }
+        public DateTime LastUpdate{get;set;}
+
     }
 }
