404039bf9f003799b45762acbbbfc9e8e88cc9ce
martin304 <ll-wang@hotmail.com>
2018-03-07 13:25:00 +0800

    E13 add VehicleFeature model

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace angular2.Models
@@ -6,10 +8,10 @@ namespace angular2.Models
     public class Vehicle
     {
         public int Id { get; set; }
-        public int ModelId{get;set;}
-        public Model Model{get;set;}
-        public bool IsRegistered{get;set;}
-       
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+
         [StringLength(255)]
         public string contactEmail { get; set; }
         [Required]
@@ -18,7 +20,11 @@ namespace angular2.Models
         [Required]
         [StringLength(255)]
         public string contactPhone { get; set; }
-        public DateTime LastUpdate{get;set;}
-
+        public DateTime LastUpdate { get; set; }
+        public ICollection<VehicleFeature> Features { get; set; }
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
     }
 }
