c07af8c634bd507275397964ba8bc962b8d1b209
martin304 <ll-wang@hotmail.com>
2018-03-07 12:56:55 +0800

    E11 add vehicle Model

---
+++ Models/Vehicle.cs
@@ -0,0 +1,10 @@
+namespace angular2.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Name{get;set;}
+        public int ModelId{get;set;}
+        public Model Model{get;set;}
+    }
+}
