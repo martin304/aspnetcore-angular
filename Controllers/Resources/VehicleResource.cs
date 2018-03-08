f4bf3d02f0ef905b6dfcfdddcd42c2ce81b15efd
martin304 <ll-wang@hotmail.com>
2018-03-08 18:07:19 +0800

    E27 Get action refactoring

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using angular2.Models;
@@ -6,17 +7,18 @@ namespace angular2.Controllers.Resources
 {
     public class VehicleResource
     {
-        public int Id { get; set; }
-        public int ModelId { get; set; }
-
+         public int Id { get; set; }
+       
+        public ModelResource Model { get; set; }
+        public MakeResource Make{get;set;}
         public bool IsRegistered { get; set; }
 
-
         public ContactResource Contact{get;set;}
-        public ICollection<int> Features { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public ICollection<Feature> Features { get; set; }
         public VehicleResource()
         {
-            Features = new Collection<int>();
+            Features = new Collection<Feature>();
         }
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -13,9 +13,14 @@ namespace angular2.Mapping
             //Domain to API Resource
             CreateMap<Make,MakeResource>();
             CreateMap<Model,ModelResource>();
-            CreateMap<Vehicle,VehicleResource>()
+            CreateMap<Feature,FeatureResource>();
+            CreateMap<Vehicle,SaveVehicleResource>()
                .ForMember(vr=>vr.Contact,opt =>opt.MapFrom(v=>new ContactResource{Name=v.contactName,Email=v.contactEmail,Phone=v.contactPhone}))
                .ForMember(vr=>vr.Features,opt=>opt.MapFrom(v=>v.Features.Select(vf=>vf.FeatureId)));
+             CreateMap<Vehicle,VehicleResource>()
+               .ForMember(vr=>vr.Contact,opt =>opt.MapFrom(v=>new ContactResource{Name=v.contactName,Email=v.contactEmail,Phone=v.contactPhone}))
+               .ForMember(vr=>vr.Features,opt=>opt.MapFrom(v=>v.Features.Select(vf=>new FeatureResource{Id=vf.Feature.Id,Name=vf.Feature.Name})))
+                 .ForMember(vr=>vr.Make,opt =>opt.MapFrom(v=>v.Model.Make));
             //API Resource to Domain
             CreateMap<VehicleResource,Vehicle>()
               .ForMember(v=>v.Id,opt=>opt.Ignore())
@@ -26,12 +31,12 @@ namespace angular2.Mapping
               .AfterMap((vr,v)=>{
                   //remove unselect features
                  
-             var  removedFeatures= v.Features.Where(f=>!vr.Features.Contains(f.FeatureId));
+             var  removedFeatures= v.Features.Where(vf=>!vr.Features.Any(f=>f.Id==vf.FeatureId));
                   foreach(var f in removedFeatures){
                       v.Features.Remove(f);
                   }
                   //add new features
-               var addedFeatures=   vr.Features.Where(id=>!v.Features.Any(vf=>vf.FeatureId==id)).Select(id=>new VehicleFeature{FeatureId=id});
+               var addedFeatures=   vr.Features.Where(f=>!v.Features.Any(vf=>vf.FeatureId==f.Id)).Select(f=>new VehicleFeature{FeatureId=f.Id });
                   foreach(var f in addedFeatures){
                           v.Features.Add(f);
                   }
