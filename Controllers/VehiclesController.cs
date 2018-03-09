6a157b6e4932b7d810409897a470e7701484aa57
martin304 <ll-wang@hotmail.com>
2018-03-09 12:08:15 +0800

    E263 modify Get Action

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -23,10 +23,13 @@ namespace angular2.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetVehicle(int id)
         {
-            var  vehicle=   await  context.Vehicles.Include(v=>v.Features).SingleOrDefaultAsync(v=>v.Id==id);
+            var  vehicle=   await  context.Vehicles
+            .Include(v=>v.Features)
+            .ThenInclude(vf=>vf.Feature)
+            .SingleOrDefaultAsync(v=>v.Id==id);
             if(vehicle==null)
             return NotFound();
-          var vehicleResource=mapper.Map<Vehicle,SaveVehicleResource>(vehicle);
+          var vehicleResource=mapper.Map<Vehicle,VehicleResource>(vehicle);
             return Ok(vehicleResource);
         }
         [HttpPost]
